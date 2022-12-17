7edbed1e13da7f1d5ff0c9f2d4dbf4c234ae343f
Dreccanus <118807753+Dreccanus@users.noreply.github.com>
2022-12-17 18:32:22 +0100

    Add files via upload

--- berserker.py
+++ berserker.py
@@ -18,10 +18,10 @@ try:
     print(CRED + "Github : https://github.com/dreccanus \nTwitter : https://twitter.com/dreccnus" + CEND)
 except():
     print ("error")
-parser = argparse.ArgumentParser(description ='Berserker Encoder !!\n python3 --encrypt [String]')
+parser = argparse.ArgumentParser(description ='Berserker Encoder/Decoder !!\n python3 --encrypt [String]\n --decrypt [string]')
 
-parser.add_argument('--encode', '-e', type = str, help ='Encode the text')
-
+parser.add_argument('--encrypt', '-e', type = str, help ='Encrypt the text')
+parser.add_argument('--decrypt', '-d', type = str, help ='Decrypt the text')
 
 
 if len(sys.argv) < 2:
@@ -31,34 +31,56 @@ else:
     args = parser.parse_args()
 
 
+def encrypt():
+    value = (args.encrypt)
+    jj = (binascii.hexlify(value.encode("utf-8")))
+    jj2 = jj.decode()
+    jj3 = (jj2[::-1])
+    output = ''
+    for c in jj3:
+        h = ("{:08b}".format(ord(c)))
 
-value = (args.encode)
-jj = (binascii.hexlify(value.encode("utf-8")))
-jj2 = jj.decode()
-jj3 = (jj2[::-1])
-output = ''
-for c in jj3:
-    h = ("{:08b}".format(ord(c)))
+        output += h
 
-    output += h
+    lmqawed = (output[::-1])
+    modified_string1 = re.sub(r'[1]', 'x', lmqawed)
+    modified_string1 = re.sub(r'[0]', ':', modified_string1)
+    finiiisyo = (modified_string1)
+    def run_length_encode(string):
+        encoded_string = ""
+        current_char = string[0]
+        char_count = 1
+        for i in range(1, len(string)):
+            if string[i] == current_char:
+                char_count += 1
+            else:
+                encoded_string += current_char + str(char_count)
+                current_char = string[i]
+                char_count = 1
+        encoded_string += current_char + str(char_count)
+        return encoded_string
 
-lmqawed = (output[::-1])
-modified_string1 = re.sub(r'[1]', 'x', lmqawed)
-modified_string1 = re.sub(r'[0]', ':', modified_string1)
-finiiisyo = (modified_string1)
-def run_length_encode(string):
-    encoded_string = ""
-    current_char = string[0]
-    char_count = 1
-    for i in range(1, len(string)):
-        if string[i] == current_char:
-            char_count += 1
-        else:
-            encoded_string += current_char + str(char_count)
-            current_char = string[i]
-            char_count = 1
-    encoded_string += current_char + str(char_count)
-    return encoded_string
+    string = (finiiisyo)
+    print ("==> "+string)
 
-string = (finiiisyo)
-print (string)
+
+def decrypt():
+    try:
+        string = args.decrypt
+        modified_string1 = re.sub(r'[x]', '1', string)
+        modified_string1 = re.sub(r'[:]', '0', modified_string1)
+        sting_rv = modified_string1[::-1]
+        steee2nin = (sting_rv)
+        hex_string = ''.join(hex(int(x, 2))[2:] for x in steee2nin.split())
+        decoded_string = binascii.unhexlify(hex_string).decode()
+        wwe = (decoded_string[::-1])
+        decoded_string = bytes.fromhex(wwe).decode()
+        print ("==> "+decoded_string)
+    except(ValueError):
+        print ("Syntax error")
+
+if args.encrypt:
+    encrypt()
+
+elif args.decrypt:
+    decrypt()
